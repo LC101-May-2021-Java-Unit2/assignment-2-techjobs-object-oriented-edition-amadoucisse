ece5e5b408647ce703c96046faf5070873eba76a
amadoucisse <84243590+amadoucisse@users.noreply.github.com>
2021-09-16 21:25:33 -0500

    adding tests

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,45 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
+import org.launchcode.techjobs.oo.*;
+import static org.junit.Assert.*;
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
 
+
+    @Test
+    public void testSettingJobId() {
+     Job job_1 = new Job();
+     Job job_2 = new Job();
+     assertNotEquals(job_1.getId(), job_2.getId());
+    }
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals("Product tester", job.getName().toString());
+        assertEquals("ACME", job.getEmployer().toString());
+        assertEquals("Desert", job.getLocation().toString());
+        assertEquals("Quality control", job.getPositionType().toString());
+        assertEquals("Persistence", job.getCoreCompetency().toString());
+        assertTrue(job instanceof Job);
+        assertTrue(job.getEmployer() instanceof Employer);
+        assertTrue(job.getLocation() instanceof Location);
+        assertTrue(job.getPositionType() instanceof PositionType);
+        assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
+    }
+    @Test
+    public void testJobsForEquality() {
+        Job job_1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+        Job job_2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+
+        assertFalse(job_1.equals(job_2));
+    }
 }
